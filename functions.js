d548131e401e93be940c24c68fa32507642d1507
frederikwalter <frederikpwalter@gmail.com>
2023-03-04 10:11:03 +0100

    Upload homepage from cs50 class
    Upload the homepage as submitted during week 8 of cs50 class.

---
+++ functions.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#swim').addEventListener('click', function() {
+      alert("excellent choice");
+    });
+
+    document.querySelector('#ski').addEventListener('click', function() {
+        alert("excellent choice");
+      });
+
+    document.querySelector('#run').addEventListener('click', function() {
+    alert("excellent choice");
+    });
+
+    document.querySelector('#bike').addEventListener('click', function() {
+    alert("excellent choice");
+    });
+
+  });
